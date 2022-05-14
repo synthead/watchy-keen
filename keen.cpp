be69f7309e13218328b16b193850c10ffd74919c
Maxwell Pray <synthead@gmail.com>
2022-05-14 02:37:59 -0700

    Add fancy battery meter with 27 steps.

--- keen.cpp
+++ keen.cpp
@@ -18,9 +18,19 @@
 #define BATTERY_X 165
 #define BATTERY_Y 180
 
+#define BATTERY_SEGMENTS 3
 #define BATTERY_SEGMENT_WIDTH 7
 #define BATTERY_SEGMENT_HEIGHT 11
 #define BATTERY_SEGMENT_SPACING 9
+
+#define BATTERY_SWEEP_MAX 13
+#define BATTERY_SWEEP_TRANSITION 7
+#define BATTERY_SWEEP_STEPS (BATTERY_SWEEP_MAX + (BATTERY_SWEEP_TRANSITION * (BATTERY_SEGMENTS - 1)))
+
+#define BATTERY_VOLTAGE_FULL 4.1
+#define BATTERY_VOLTAGE_EMPTY 3.7
+#define BATTERY_VOLTAGE_RANGE (BATTERY_VOLTAGE_FULL - BATTERY_VOLTAGE_EMPTY)
+#define BATTERY_VOLTAGE_STEP BATTERY_VOLTAGE_RANGE / BATTERY_SWEEP_STEPS
 
 time_t epoch;
 
@@ -105,19 +115,25 @@ void WatchyKeen::drawWeekday() {
 
 void WatchyKeen::drawBattery() {
   float voltage = getBatteryVoltage();
-  uint8_t level = 0;
+  int bar_index = 0;
 
-  if (voltage > 4.1) {
-    level = 3;
-  } else if (voltage > 3.95) {
-    level = 2;
-  } else if (voltage > 3.80) {
-    level = 1;
+  for (; bar_index < BATTERY_SWEEP_STEPS; bar_index++) {
+    float voltage_test = BATTERY_VOLTAGE_STEP * bar_index + BATTERY_VOLTAGE_EMPTY;
+
+    if (voltage_test >= voltage) {
+      break;
+    }
   }
 
-  for (uint8_t segment = 0; segment < level; segment++) {
+  uint8_t bar_indexes[BATTERY_SEGMENTS];
+  bar_indexes[0] = min(bar_index, BATTERY_SWEEP_MAX);
+  bar_indexes[1] = max(min(bar_index - BATTERY_SWEEP_TRANSITION, BATTERY_SWEEP_MAX), 0);
+  bar_indexes[2] = max(bar_index - BATTERY_SWEEP_TRANSITION * 2, 0);
+
+  for (uint8_t segment = 0; segment < BATTERY_SEGMENTS; segment++) {
+    const unsigned char *battery_meter_bar = battery_meter_bars[bar_indexes[segment]];
     uint8_t battery_x = BATTERY_X + (segment * BATTERY_SEGMENT_SPACING);
 
-    display.fillRect(battery_x, BATTERY_Y, BATTERY_SEGMENT_WIDTH, BATTERY_SEGMENT_HEIGHT, GxEPD_BLACK);
+    display.drawBitmap(battery_x, BATTERY_Y, battery_meter_bar, BATTERY_SEGMENT_WIDTH, BATTERY_SEGMENT_HEIGHT, GxEPD_BLACK);
   }
 }
